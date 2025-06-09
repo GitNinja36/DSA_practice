a0b4b0cfc604d0778f764f87be057a711740a781
Rohit27 <137439791+GitNinja36@users.noreply.github.com>
2025-06-09 17:52:54 +0530

    solve some question on tree

--- BT.java
+++ BT.java
@@ -178,7 +178,96 @@ public class BT {
         return true;
     }
 
-    //
+    //Kth level of tree
+    public static void Klevel(Node root, int level, int k){
+        if(root == null){
+            return;
+        }
+        if(level == k){
+            System.out.print(root.data +" ");
+            return;
+        }
+        Klevel(root.left, level+1, k);
+        Klevel(root.right, level+1, k);
+    }
+
+    //lowest common ancestor
+    public static Node lca(Node root, int n1, int n2){
+        ArrayList<Node> path1 = new ArrayList<>();
+        ArrayList<Node> path2 = new ArrayList<>();
+
+        getPath(root, n1, path1);
+        getPath(root, n2, path2);
+        int i = 0;
+        for(; i < path1.size() && i < path2.size(); i++){
+            if(path1.get(i) != path2.get(i)){
+                break;
+            }
+        }
+
+        return path1.get(i - 1);
+    }
+    public static boolean getPath(Node root, int n, ArrayList<Node> path) {
+        if( root == null){
+            return false;
+        }
+        path.add(root);
+        if(root.data == n){
+            return true;
+        }
+        boolean foudLeft = getPath(root.left, n, path);
+        boolean foudRight = getPath(root.right, n, path);
+        if(foudLeft || foudRight){
+            return true;
+        }
+        path.remove(path.size()-1);
+        return false;
+    }
+
+    //lowest common ancestor approach - 2
+    public static Node lca2(Node root, int n1, int n2){
+        if(root == null || root.data == n1 || root.data == n2){
+            return root;
+        }
+        Node leftLca = lca2(root.left, n1, n2);
+        Node rightLca = lca2(root.right, n1, n2);
+
+        //Case 1
+        if(rightLca == null){
+            return leftLca;
+        }
+        //case 2
+        if(leftLca == null){
+            return rightLca;
+        }
+        return root;
+    }
+
+    public static int lcaDist(Node root, int n){
+        if(root == null){
+            return -1;
+        }
+        if(root.data == n){
+            return 0;
+        }
+        int leftDist = lcaDist(root.left, n);
+        int rightDist = lcaDist(root.right, n);
+
+        if(leftDist == -1 && rightDist == -1){
+            return -1;
+        }else if(leftDist == -1){
+            return rightDist + 1;
+        }else{
+            return leftDist + 1;
+        }
+    }
+    //minimum distance
+    public static int minDistance(Node root, int n1, int n2){
+        Node lca = lca2(root, n1, n2);
+        int dist1 = lcaDist(lca, n1);
+        int dist2 = lcaDist(lca, n2);
+        return dist1 + dist2; 
+    }
 
     public static void main(String[] args) {
         // int nodes[] = {1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1};
@@ -208,6 +297,9 @@ public class BT {
         // System.out.println("sum of Nodes : " + sumNode(root));
         // System.out.println("maximum diameter : " + diameter(root));
         // System.out.println("maximum diameter approach 2 : " + diamete2(root).diameter);
-        System.out.println(isSubtree(root, subRoot));
+        // System.out.println(isSubtree(root, subRoot));
+        // Klevel(root, 1, 2);
+        // System.out.println(lca(root, 1, 2).data);
+        System.out.println(minDistance(root, 4, 6));
     }
 }
